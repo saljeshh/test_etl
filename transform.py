358d96a6869f234b226c96209951d96c617c0adf
saljeshh <saljesh2020@gmail.com>
2024-06-25 10:53:10 +0545

    added data and todos

--- transform.py
+++ transform.py
@@ -2,6 +2,40 @@
 def transform():
     
     
+    data = [
+        {
+            "name": "Avengers",
+            "Rating": "8",
+            "id": "568754",
+            "year": 2022,
+            "hawa": 'ajfkadsjfioadwjfoiadsjf',
+            "desc": " "
+        },
+        {
+            "name": "Ragini MMS",
+            "Rating": "3",
+            "id": "39393",
+            "year": 2018,
+            "hawa": 'ruy 9ew8ruiadshfasdhfjadsnfkjnadkj',
+            "desc": " "
+        }
+    ]
+    
+    # conver to df
+    
+    # todo: change datatype 
+    
+    
+    # todo: drop unwanted columns
+    
+    
+    # todo: return new data
+    
+    
+    
+    
     transformed_data = []
+    
+    
     
     return transformed_data
